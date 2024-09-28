212b674857d924ceff6f6b20bc843a0bbafd12f8
jgjuara <jg.juara@gmail.com>
2024-09-28 20:24:41 -0300

    fix asignacion de data

--- src-juara/predecir_lgbm.py
+++ src-juara/predecir_lgbm.py
@@ -1,7 +1,7 @@
 import funciones_lgbm
 import lgbm_globales
 
-X_train, y_train_binaria2, y_train_binaria1, w_train, X_test, y_test_class, w_test = preparar_data(dataset_path, dataset_file, mes_train = [202102, 202103, 202104], mes_test = 202106)
+X_train, y_train_binaria1, y_train_binaria2, w_train, X_test, y_test_class, y_test_binaria1, w_test = preparar_data(dataset_path, dataset_file, mes_train = [202102, 202103, 202104], mes_test = 202106)
 
 
 study = optuna.create_study(
